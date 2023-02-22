4a7e3068c6ce40b54ff1b3c9860fca4105dab87e
Ankita <chaudharyankita755@gmail.com>
2023-02-22 22:56:39 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Progress from "./Progress";
+import Form from "./Form";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <Header />
+      <div className="page_body">
+        <div className="progress">
+          <Progress />
+        </div>
+      </div>
+
+      <div className="form_body">
+        <div className="form1">
+          <Form />
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
